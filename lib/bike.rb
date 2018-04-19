565e6b86c2f1d9dad17ac44c94589fbd292188af
George Sykes <georgesykes86@gmail.com>
2018-04-19 12:56:10 +0100

    Adding broken attribute and ability to report to Bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,17 @@
 class Bike
+
+  attr_reader :broken
+
+  def initialize(bike_status = true)
+    @broken = bike_status
+  end
+
   def working?
-    true
+    !broken
+  end
+
+  def report_broken
+    @broken = true
   end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,24 @@ describe Bike do
 
   it {is_expected.to respond_to(:working?)}
 
+  describe '#working' do
+    let(:bike) {Bike.new}
+
+    context 'when the bike is working' do
+      before { bike.broken = false }
+      it 'shows that it is working' do
+        expect(bike.working?).to be true
+      end
+
+    end
+
+    context 'when the bike is broken' do
+      before { bike.broken = true }
+      it 'shows that it is not working' do
+        expect(bike.working?).to be false
+      end
+
+    end
+  end
+
 end
